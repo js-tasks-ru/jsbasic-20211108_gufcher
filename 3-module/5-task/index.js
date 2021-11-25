9d51a5b445ec1ad20e2c5b145d4f2c76775f8cbb
Vladimir Kuprin <Gufcher@gmail.com>
2021-11-25 20:19:00 +0700

    module 3 task done

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,19 @@
 function getMinMax(str) {
-  // ваш код...
+  let arrayFromString = str.split(' ').map(element => +element).filter(element => !isNaN(element));
+  let maxValue = arrayFromString[0];
+  let minValue = arrayFromString[0];
+  for (let key of arrayFromString) {
+    if (key > maxValue) {
+      maxValue = key;
+    }
+  }
+  for (let key of arrayFromString) {
+    if (key < minValue) {
+      minValue = key;
+    }
+  }
+  return {
+    min: minValue,
+    max: maxValue,
+  };
 }
